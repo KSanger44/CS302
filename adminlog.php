4bc111454db66c7112c778376bfc1d563fcbd781
Kyle Sanger <ksanger@edgewood.edu>
2023-04-09 16:23:28 -0500

    admin log

--- adminlog.php
+++ adminlog.php
@@ -21,15 +21,45 @@
                 </div>
                 <div class="form-group">
                     <label for="pw">Password:</label><br>
-                    <input type="password" id="pw" name="pw" placeholder="****" value="">
+                    <input type="password" id="password" name="password" placeholder="****" value="">
                 </div><br>
                 
                 <div class="form-group">
-                    <button><a href='control.php' style="border:none;text-decoration:none; color: black;">Submit</a></button>
+                    <input type="submit" class="btnSubmit" value="Login" />
                 </div>
             </form>
         </div>
     </div>
+
+    <?php
+    include("config.php");
+    session_start();
+   
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+        
+        $user = mysqli_real_escape_string($conn,$_POST['username']);
+        $password = mysqli_real_escape_string($conn,$_POST['password']);
+
+        $sql = "SELECT * FROM admin WHERE username = '$user' and password = '$password'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+        $count = mysqli_num_rows($result);
+        // If result matched $email and $password, table row must be 1 row
+
+        if($count == 1) {
+            //session_register("myusername");
+               $_SESSION['adminID'] = $adminID;
+               $_SESSION['username'] = $username;
+               $error = "";
+            
+               header("location: index.php");
+           }else {
+               echo "Your Login Email or Password is invalid";
+         }
+      }
+    ?>    
 </div>
 </body>
 </html>
