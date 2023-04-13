24a543683607b6adad11f4691b8bec86bad5d2c9
Kyle Sanger <ksanger@edgewood.edu>
2023-04-13 18:58:31 -0500

    no echo on

--- control.php
+++ control.php
@@ -159,7 +159,7 @@
             }
             
             $status1 = isset($_POST['status1']) ? $_POST['status1'] : "";
-            echo $status1;
+            //echo $status1;
 
             
             if(isset($_POST['confirm1'])&& $status1 != ""){
