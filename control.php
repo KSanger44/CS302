458dbe536c4abd1fc8642bc77928d6739a142329
Kyle Sanger <ksanger@edgewood.edu>
2023-04-15 14:00:21 -0500

    alert procedure

--- control.php
+++ control.php
@@ -264,7 +264,17 @@
                            VALUES (NULL, 'd', '$procID', '0', '$date')";
 
             if (mysqli_query($conn, $updatesql)) {
-              echo "Record Updated successfully.";
+              echo "Status Updated successfully.";
+              $URL="http://localhost/dashboard/302//CS302/control.php";
+              echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
+              echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
+                            
+            } else {
+                echo "Error: " . $updatesql . "<br>" . mysqli_error($conn);
+            }
+
+            if (mysqli_query($conn, $alertsql)) {
+              echo "Alert added successfully.";
               $URL="http://localhost/dashboard/302//CS302/control.php";
               echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
               echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
